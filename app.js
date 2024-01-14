d79ded7571735dac797d58fbcd690cb200f5af30
Brant Han <brant.han@na.ri.ad>
2024-01-13 20:31:45 -0800

    costco list

--- app.js
+++ app.js
@@ -1,81 +1,390 @@
 const menu = [
   {
     id: 1,
-    title: "buttermilk pancakes",
-    category: "design",
+    title: "Apples",
+    status: "all",
     price: 15.99,
-    img: "./images/item-1.jpeg",
-    desc: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `,
+    img: "./images/jld-envy-2860.jpg",
+    desc: `apple`,
   },
   {
     id: 2,
-    title: "diner double",
-    category: "javascript",
+    title: "Sausages",
+    status: "all",
     price: 13.99,
-    img: "./images/item-2.jpeg",
-    desc: `vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats `,
+    img: "./images/apple-sausage.png",
+    desc: `apple sausage`,
   },
   {
     id: 3,
-    title: "godzilla milkshake",
-    category: "api",
+    title: "Small Trash Bags",
+    status: "all",
     price: 6.99,
-    img: "./images/item-3.jpeg",
-    desc: `ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.`,
+    img: "./images/baby trash.webp",
+    desc: `baby trash bags`,
   },
   {
     id: 4,
-    title: "country delight",
-    category: "design",
+    title: "Baby Wipes",
+    status: "all",
     price: 20.99,
-    img: "./images/item-4.jpeg",
-    desc: `Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut, `,
+    img: "./images/baby-wipes.png",
+    desc: `babywipes`,
   },
   {
     id: 5,
-    title: "egg attack",
-    category: "javascript",
+    title: "Batteries",
+    status: "all",
     price: 22.99,
-    img: "./images/item-5.jpeg",
-    desc: `franzen vegan pabst bicycle rights kickstarter pinterest meditation farm-to-table 90's pop-up `,
+    img: "./images/batteries.png",
+    desc: `batteries `,
   },
   {
     id: 6,
-    title: "oreo dream",
-    category: "api",
+    title: "Short Rib",
+    status: "all",
     price: 18.99,
-    img: "./images/item-6.jpeg",
-    desc: `Portland chicharrones ethical edison bulb, palo santo craft beer chia heirloom iPhone everyday`,
+    img: "./images/braised-beef.png",
+    desc: `braised beef short rib`,
   },
   {
     id: 7,
-    title: "bacon overflow",
-    category: "design",
+    title: "Burgers",
+    status: "all",
     price: 8.99,
-    img: "./images/item-7.jpeg",
-    desc: `carry jianbing normcore freegan. Viral single-origin coffee live-edge, pork belly cloud bread iceland put a bird `,
+    img: "./images/angus-burger.png",
+    desc: `burgers `,
   },
   {
     id: 8,
-    title: "american classic",
-    category: "javascript",
+    title: "Carrot Juice",
+    status: "all",
     price: 12.99,
-    img: "./images/item-8.jpeg",
-    desc: `on it tumblr kickstarter thundercats migas everyday carry squid palo santo leggings. Food truck truffaut  `,
+    img: "./images/bolthouse-farms-organic-carrot-juice-3-x-32-fl-oz.png",
+    desc: `carrot juice`,
   },
   {
     id: 9,
-    title: "quarantine buddy",
-    category: "api",
+    title: "Chicken Bake",
+    status: "all",
     price: 16.99,
-    img: "./images/item-9.jpeg",
-    desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+    img: "./images/chicken-bake.png",
+    desc: `cbake`,
   },
+  {
+    id: 10,
+    title: "Cholula",
+    status: "all",
+    price: 16.99,
+    img: "./images/cholula.png",
+    desc: `cholula`,
+  },
+  {
+    id: 11,
+    title: "Cheese",
+    status: "all",
+    price: 16.99,
+    img: "./images/finlandia.png",
+    desc: `chz`,
+  },
+  {
+    id: 12,
+    title: "Coastal Berry",
+    status: "all",
+    price: 16.99,
+    img: "./images/coastal-berry.png",
+    desc: `Coastal`,
+  },
+  {
+    id: 13,
+    title: "Coffee",
+    status: "all",
+    price: 16.99,
+    img: "./images/coffee.png",
+    desc: `coffee`,
+  },
+  {
+    id: 14,
+    title: "Collagen",
+    status: "all",
+    price: 16.99,
+    img: "./images/collagen.png",
+    desc: `collagen`,
+  },
+  {
+    id: 15,
+    title: "Canadian Bacon",
+    status: "all",
+    price: 16.99,
+    img: "./images/jones-dairy-farm-uncured-canadian-bacon-slices-24-oz-1.png",
+    desc: `cooked ham for tortillae`,
+  },
+  {
+    id: 16,
+    title: "Eggs",
+    status: "all",
+    price: 16.99,
+    img: "./images/eggs.png",
+    desc: `eggz`,
+  },
+  {
+    id: 17,
+    title: "Filet Mignon",
+    status: "all",
+    price: 16.99,
+    img: "./images/filet-mignon.png",
+    desc: `filet mignon`,
+  },
+  {
+    id: 18,
+    title: "Fried Rice",
+    status: "all",
+    price: 16.99,
+    img: "./images/fried rice.png",
+    desc: `fried rice packets`,
+  },
+  {
+    id: 19,
+    title: "Garlic chicken pasta",
+    status: "all",
+    price: 16.99,
+    img: "./images/garlic-chicken.png",
+    desc: `garlic chicken pasta frozen`,
+  },
+  {
+    id: 20,
+    title: "Golden kiwi",
+    status: "all",
+    price: 16.99,
+    img: "./images/golden-kiwi.png",
+    desc: `Golden kiwi`,
+  },
+  {
+    id: 21,
+    title: "Chocolate",
+    status: "all",
+    price: 16.99,
+    img: "./images/habi-choco.png",
+    desc: `Habi chocos`,
+  },
+  {
+    id: 22,
+    title: "Ice cream",
+    status: "all",
+    price: 16.99,
+    img: "./images/jonny-pops.png",
+    desc: `Ice cream`,
+  },
+  {
+    id: 23,
+    title: "Izze",
+    status: "all",
+    price: 16.99,
+    img: "./images/izze.png",
+    desc: `izze`,
+  },
+  {
+    id: 24,
+    title: "Jelly",
+    status: "all",
+    price: 16.99,
+    img: "./images/fruitsnacks.png",
+    desc: `jelly`,
+  },
+  {
+    id: 25,
+    title: "Kaka",
+    status: "all",
+    price: 16.99,
+    img: "./images/jalapeno.png",
+    desc: `kaka`,
+  },
+  {
+    id: 26,
+    title: "Meatballs",
+    status: "all",
+    price: 16.99,
+    img: "./images/meatballs.png",
+    desc: `meatballs`,
+  },
+  {
+    id: 27,
+    title: "Milk",
+    status: "all",
+    price: 16.99,
+    img: "./images/milk.png",
+    desc: `milk`,
+  },
+  {
+    id: 28,
+    title: "Oranges",
+    status: "all",
+    price: 16.99,
+    img: "./images/oranges.png",
+    desc: `oranges`,
+  },
+  {
+    id: 29,
+    title: "Paper plates",
+    status: "all",
+    price: 16.99,
+    img: "./images/plates.png",
+    desc: `paper plates`,
+  },
+  {
+    id: 30,
+    title: "Pasta noodles",
+    status: "all",
+    price: 16.99,
+    img: "./images/pasta-noodles.png",
+    desc: `pasta nood`,
+  },
+  {
+    id: 31,
+    title: "Raos",
+    status: "all",
+    price: 16.99,
+    img: "./images/raos.png",
+    desc: `pasta sz`,
+  },
+  {
+    id: 32,
+    title: "PB pretzel",
+    status: "all",
+    price: 16.99,
+    img: "./images/pbpretz.png",
+    desc: `pb pretzel`,
+  },
+  {
+    id: 33,
+    title: "Potpies",
+    status: "all",
+    price: 16.99,
+    img: "./images/potpie.png",
+    desc: `potpies`,
+  },
+  {
+    id: 34,
+    title: "Raspberries",
+    status: "all",
+    price: 16.99,
+    img: "./images/raspberries.png",
+    desc: `raspberry`,
+  },
+  {
+    id: 35,
+    title: "Refresh tears",
+    status: "all",
+    price: 16.99,
+    img: "./images/refresh.png",
+    desc: `refresh tears`,
+  },
+  {
+    id: 36,
+    title: "Salt",
+    status: "all",
+    price: 16.99,
+    img: "./images/salt.png",
+    desc: `salt`,
+  },
+  {
+    id: 37,
+    title: "Creamer",
+    status: "all",
+    price: 16.99,
+    img: "./images/creamer.png",
+    desc: `sbux creamer`,
+  },
+  {
+    id: 38,
+    title: "Simply granola",
+    status: "all",
+    price: 16.99,
+    img: "./images/simply.png",
+    desc: `simply granola`,
+  },
+  {
+    id: 39,
+    title: "Soap bars",
+    status: "all",
+    price: 16.99,
+    img: "./images/soap.png",
+    desc: `soap bars`,
+  },
+  {
+    id: 40,
+    title: "Soba noodle",
+    status: "all",
+    price: 16.99,
+    img: "./images/soba.png",
+    desc: `soba noodle packets`,
+  },
+  {
+    id: 41,
+    title: "Strawberries",
+    status: "all",
+    price: 16.99,
+    img: "./images/strawberry.png",
+    desc: `Strawberry`,
+  },
+  {
+    id: 42,
+    title: "Swiffer wipes",
+    status: "all",
+    price: 16.99,
+    img: "./images/swiffer.png",
+    desc: `swiffer`,
+  },
+  {
+    id: 43,
+    title: "Toothpaste",
+    status: "all",
+    price: 16.99,
+    img: "./images/toothpaste.png",
+    desc: `toothpaste`,
+  },
+  {
+    id: 44,
+    title: "Tortilla",
+    status: "all",
+    price: 16.99,
+    img: "./images/tortilla.png",
+    desc: `tortilla`,
+  },
+  {
+    id: 45,
+    title: "Large trash bags",
+    status: "all",
+    price: 16.99,
+    img: "./images/trash.png",
+    desc: `trash bags`,
+  },
+  {
+    id: 46,
+    title: "Turmeric`",
+    status: "all",
+    price: 16.99,
+    img: "./images/turmeric.png",
+    desc: `turmeric`,
+  },
+  {
+    id: 47,
+    title: "Zipfizz",
+    status: "all",
+    price: 16.99,
+    img: "./images/zpfz.png",
+    desc: `zipfz`,
+  },
+  
 ];
+
+const pickedArray = [];
 
 const sectionCenter = document.querySelector(".section-center");
 
 const filterBtns = document.querySelectorAll(".filter-btn");
+console.log(filterBtns);
+
 
 
 function displayAMenu(menuItems) {
@@ -85,32 +394,66 @@ function displayAMenu(menuItems) {
   })
   console.log(displayMenu.join(""));
   sectionCenter.innerHTML = displayMenu.join("");
+
+  //console.log(menuItems);
+  // menuItems.forEach(item =>{
+  //   item.addEventListener("click", function(e){
+  //   console.log(e.currentTarget);
+  //   });
+  // })
+  
 }
 
+
+
 window.addEventListener("DOMContentLoaded", () => {
+
+  
   console.log("shake bake");
   displayAMenu(menu);
+
+  const menuItems = document.querySelectorAll(".photo");
+  menuItems.forEach(item => {
+    console.log(item);
+
+    //for each menu item, grab the child of menu-item's  (img) alt value, and create
+    //the attribute "data-key" then the value "alt value"
+    item.addEventListener("click", (e) => {
+    const target = e.currentTarget.getAttribute("alt");
+    console.log(target);
+   menu.filter(item =>{
+      console.log(item.title);
+      if (item.title === target){
+        pickedArray.push(item);
+      }
+    })
+    console.log(pickedArray);
+    })
+  });
 })
+
+
 
 filterBtns.forEach(btn => {
   btn.addEventListener("click", (e) => {
-  console.log(e.currentTarget.dataset.id);
-  const category = e.currentTarget.dataset.id;
+  console.log(e.currentTarget.dataset.key);
+  const targetTop = e.currentTarget.dataset.key;
 
   const filteredMenu = menu.filter(item => {
-    console.log(item.category);
-    if (item.category === category) {
+    console.log(item.status);
+    if (item.status === targetTop) {
     return item;
     }
   })
   console.log(filteredMenu);
 
-  if (category === "all") {
-    displayAMenu(menu);
+  if (targetTop === "all") {
+    location.reload();
   }
  else{
-  displayAMenu(filteredMenu);
+  displayAMenu(pickedArray);
  }
   })
 })
+
 
