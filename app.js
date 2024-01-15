f0e2ac5b2a1a9cd1a02b9fe1993908b1f6934ec8
Brant Han <brant.han@na.ri.ad>
2024-01-15 10:45:13 -0800

    add banana

--- app.js
+++ app.js
@@ -383,6 +383,14 @@ const menu = [
     img: "./images/zpfz.png",
     desc: `zipfz`,
   },
+  {
+    id: 48,
+    title: "Banana",
+    status: "all",
+    price: 16.99,
+    img: "./images/bunch-bananas-isolated-on-white-600nw-1722111529.png",
+    desc: `banana`,
+  },
   
 ];
 
